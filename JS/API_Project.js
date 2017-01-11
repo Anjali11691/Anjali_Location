cd69ef39adab5608f8b0067bfb8d95959d220ba9
BrieucKyo <brieucmailpro@gmail.com>
2017-01-11 19:34:24 +0100

    Changed html css from junkgo

--- JS/API_Project.js
+++ JS/API_Project.js
@@ -1,11 +1,11 @@
 var domready = function(){
-    var _u = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.870627,2.366366&radius=500&types=food&name=fast-food&key=AIzaSyA46nGujFrRxs0w9xCr0VW1_nxzdzQ6riU"
+    var _u = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.870627,2.366366&radius=500&types=food&name=fast-food&key=AIzaSyA46nGujFrRxs0w9xCr0VW1_nxzdzQ6riU";
     $.get(_u, function(data){
        
        
         for(var i=0; i<data.results.length; i++){
             var _result = data.results[i];
-            $('#food ul').append('<li>'+ _result.name + '</li>');
+            $('#results ul').append('<li>'+ _result.name + '</li>');
         }
     });
 };
